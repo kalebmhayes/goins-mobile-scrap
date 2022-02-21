865893511b3293991705a8fea76b872c16d7d7e5
kalebmhayes <khayespaperless@gmail.com>
2022-02-20 21:57:02 -0500

    media query changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,12 +7,14 @@ function Header(){
     return(
         <nav className='navbar'>
                 <div className='nav-title-box'>
-                <Link to='/'><img className='nav-photo' src='images/GMS-Logo.svg' width='300px'></img></Link>
-                <h1 className='title'>Goins Mobile Scrap</h1>
-                <div className='nav-contact'>
-                    <span><img src={Phone} width='30px' /> <span>(843)-447-0460</span></span>
-                   <span><img src={Pin} width='25px'/> <span>4887 Sawgrass Lane, Longs, SC</span> </span>
-                </div>
+                    <Link to='/'><img className='nav-photo' src='images/GMS-Logo.svg' width='300px'></img></Link>
+                    <h1 className='title'>Goins Mobile Scrap</h1>
+
+                    <div className='nav-contact'>
+                        <span><img src={Phone} width='30px' /> <span>(843)-447-0460</span></span>
+                        <span><img src={Pin} width='25px'/> <span>4887 Sawgrass Lane, Longs, SC</span> </span>
+                    </div>
+                    
             </div>
 
             <div className='nav-items'>
--- src/Components/ServicesInfo.jsx
+++ src/Components/ServicesInfo.jsx
@@ -9,7 +9,7 @@ export default function ServicesInfo(){
             <img className='services-image' src='images/services-image.jpg'/>  
 
             <div className='services-info'>
-                <p>with GMS services we have streamlined our pickp process to make it the most convenient for you.  Simply stated "You call, we haul, no job to big or to small".  We will come to you with our metal scale, and pay you that day.  See below for our list of prices.</p>
+                <p>With GMS services we have streamlined our pickp process to make it the most convenient for you.  Simply stated <span>"You call, we haul, no job to big or to small"</span>.  We will come to you with our metal scale, and pay you that day.  See below for our list of prices.</p>
                 <span className='quote'>"We Weigh, We Pay, Cash That Day"</span>
                 <p>GMS operates in Horry County, South Carolina and services from Myrtle Beach to Conway.  Our business hours are Monday-Friday from 7:00AM to 6:00PM  We offer same day cash payment for your scrap metal.  Scrap metal prices are subject to change each month, so it is best to call if you have any questions.</p>
                 <p>If you are an individual or business with very large pickups, for example large AC units, please call 2 days in advance to schedule a pickup.  We value and appreciate your business, and would like to thank you for choosing Goins Mobile Scrap.</p>
